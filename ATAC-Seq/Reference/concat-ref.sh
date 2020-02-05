32d80345bd0ae6fc3221a32ef7b3852ff5f32adb
Jason Bacon <bacon@login.peregrine.hpc.uwm.edu>
2020-02-05 17:00:05 -0600

    Get BWA running

--- ATAC-Seq/Reference/concat-ref.sh
+++ ATAC-Seq/Reference/concat-ref.sh
@@ -7,7 +7,9 @@ for variant in "" "_rm" "_sm"; do
 	files="$files Mus_musculus.GRCm38.dna$variant.chromosome.$chr.fa.gz"
 	chr=$(( $chr + 1 ))
     done
-    autosome_file=Mus_musculus.GRCm28.dna$variant.autosomes.fa.gz
-    printf "Concatenating $files to $autosome_file...\n"
-    gzcat $files | gzip - > $autosome_file
+    autosome_file=Mus_musculus.GRCm38.dna$variant.autosomes.fa
+    if [ ! -e $autosome_file ]; then
+	printf "Concatenating $files to $autosome_file...\n"
+	gzcat $files > $autosome_file
+    fi
 done
