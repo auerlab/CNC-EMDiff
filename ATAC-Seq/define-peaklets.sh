24c749adfae7528cf544302b71ff87e2ba9d4f86
Jason Bacon <bacon@danio.uits.uwm.edu>
2020-02-21 08:09:16 -0600

    Clean up peaklet refinement

--- ATAC-Seq/define-peaklets.sh
+++ ATAC-Seq/define-peaklets.sh
@@ -25,9 +25,14 @@ if [ $# != 0 ]; then
     usage
 fi
 
+# https://bedtools.readthedocs.io/en/latest/content/tools/slop.html
+# explains nicely why you should use bedtools rather than just awk this.
+# 
 #awk '{ printf("%s\t%s\t%s\t%s\t%s\n", $1, $2-250, $2+250, $4, $5) }' \
 #    ATAC.nodup.unique.macs.peaklets_summits.bed \
 #    > ATAC.nodup.unique.macs.peaklets-500.bed
+
+cd 7-macs-peaklets
 
 ##########################################################################
 #   Filter for high-confidence peaks: p-value < 1e-10
@@ -35,7 +40,8 @@ fi
 
 awk '$8 >= 10 { print $0 }' ATAC.nodup.unique.macs.peaklets_peaks.narrowPeak \
     > high-confidence-narrow.bed
-awk -f filter.awk -v file_to_filter=ATAC.nodup.unique.macs.peaklets_summits.bed \
+awk -f ../filter.awk \
+    -v file_to_filter=ATAC.nodup.unique.macs.peaklets_summits.bed \
     high-confidence-narrow.bed > high-confidence-summits.bed
 
 ##########################################################################
@@ -60,3 +66,8 @@ printf "\nWord counts:\n"
 wc ATAC.nodup.unique.macs.peaklets_summits.bed high-confidence-summits.bed \
     high-confidence-500-merged.bed
 
+read -p "Remove intermediate files? [y]/n " cleanup
+if [ 0$cleanup != 0n ]; then
+    rm -f high-confidence-narrow.bed high-confidence-summits.bed \
+	high-confidence-500.bed
+fi
