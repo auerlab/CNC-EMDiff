ff14a236a0eda8d0b6e7646ab6b386138a16da23
Jason Bacon <bacon@danio.uits.uwm.edu>
2020-03-28 20:21:09 -0500

    Clean up DESeq2 script

--- ATAC-Seq/10-diff-anal.R
+++ ATAC-Seq/10-diff-anal.R
@@ -1,3 +1,13 @@
+############################################################################
+#   CNC-EMDiff differential peak analysis
+#   Jason W. Bacon
+#   Spring, 2020
+#
+#   Based on OpticRegen by Andrea Rau
+#   https://github.com/andreamrau/OpticRegen_2019
+#   Thanks to Paul Auer for extensive guidance
+############################################################################
+
 # Choose CCA for chondro or NCA for neuro
 args <- commandArgs(trailingOnly=TRUE)
 if ( (length(args) != 1) || ((args[1] != "CCA") && (args[1] != "NCA")) )
@@ -20,74 +30,11 @@ pause <- function()
 #    invisible(b <- scan("stdin", character(), nlines=1, quiet=TRUE))
 }
 
-# Keep print() from spewing huge tables to stdout
+# Prevent print() from spewing huge tables to stdout
 options(max.print=60)
 
-# Update Jan 26, 2018: p-value sort on transgene alignment --------------------
-# Peaks re-aligned to include transgene as well
-# Use only high-quality macs2 peaks
-# for(time in c(0,2,4,7,12)) {
-#   macsBed <- read.delim(paste0("PEAKS_TRANS/", time, 
-#                                "ATAC.nodup.unique.macs_peaks.narrowPeak"), 
-#                         header=FALSE)
-#   ## Sort by p-value ($V8), only keep those with p-value < 10^-10
-#   keep <- which(macsBed$V8 > -log(10^-10))
-#   macsBed_keep <- macsBed[keep,]
-#   cat(nrow(macsBed), "...", nrow(macsBed_keep), "\n")
-#   write.table(macsBed_keep, paste0("PEAKS_TRANS/", time, 
-#                                    "ATAC.nodup.unique.macs_peaks.pvalsort.narrowPeak.bed"), 
-#               col.names=FALSE, row.names=FALSE, quote=FALSE, sep="\t")
-#   if(time == 0) macs_all <- macsBed_keep
-#   if(time != 0) macs_all <- rbind(macs_all, macsBed_keep)
-# }
-# # 62703 ... 16447 
-# # 46864 ... 14589 
-# # 102711 ... 22400 
-# # 58181 ... 17087 
-# # 70846 ... 16684 
-#
-# ## And write out merged 
-# macs_all_GR <- GRanges(seqnames=as.vector(macs_all[,1]),
-#                        IRanges(start=as.numeric(as.vector(macs_all[,2])),
-#                                end=as.numeric(as.vector(macs_all[,3]))),
-#                        strand=rep("*", nrow(macs_all)))
-# elementMetadata(macs_all_GR) <- macs_all[,-c(1:3)]
-# colnames(elementMetadata(macs_all_GR)) <- c("Peak_ID","Score","Strand", 
-#                                             "Fold-change", "log10pvalue", "log10qvalue",
-#                                             "Relative_summit")
-## Consensus peaks: 24203 peaks
-# macs_merge <- reduce(macs_all_GR)
-# macsGR_merge_bed <- data.frame(chr=seqnames(macs_merge),
-#                                starts=start(macs_merge),
-#                                end=end(macs_merge),
-#                                names=c(rep(".", length(macs_merge))),
-#                                scores=c(rep(".", length(macs_merge))),
-#                                strands=strand(macs_merge))
-# write.table(macsGR_merge_bed, paste0("PEAKS_TRANS/", 
-#                                      "ALLMERGED_ATAC.nodup.unique.macs_peaks.pvalsort.narrowPeak.bed"), 
-#             col.names=FALSE, row.names=FALSE, quote=FALSE, sep="\t")
-#
-# ## There is a single peak on the transgene after merging: 163:5092
-# #1 99999 179 5042 0ATAC.nodup.unique.macs_peak_1 15225  . 3.18297 1527.576 1522.527 2621
-# #1 99999 286 5015 2ATAC.nodup.unique.macs_peak_1 11420  . 3.70956 1147.315 1142.016 2514
-# #1 99999 163 5092 4ATAC.nodup.unique.macs_peak_1 22038  . 3.30861 2208.807 2203.81 2637
-# #1 99999 263 5015 7ATAC.nodup.unique.macs_peak_1 14011  . 3.04765 1406.3 1401.195 2537
-# #1 99999 473 4918 12ATAC.nodup.unique.macs_peak_1 15566  . 4.09869 1562.086 1556.653 2327
-# for(time in c(0,2,4,7,12)) {
-#   tmp <- read.table(paste0("PEAKS_TRANS/", time, 
-#                            "ATAC.nodup.unique.macs_peaks.pvalsort.narrowPeak.bed"))
-#   tmp <- tmp[which(tmp$V1 == 99999),]
-#   print(tmp)
-# }
-
-# Update Sep 18, 2018: peaks called on merged time pts, include pealets -------
-# Col 10 is distance from peak start to summit
-#macsBed <- read.delim(paste0("PEAKS_TRANS_PEAKLETS_ALLTIMES/", 
-#                             "ATAC.nodup.unique.macs.peaklets_peaks.narrowPeak"), 
-#                      header=FALSE)
-
-Sys.umask(mode = 007)   # Make all output group-writable
-
+# Make all output group-writable
+Sys.umask(mode = 007)
 
 # Generate merged peaks file
 # Done externally using bedtools instead if FALSE here
@@ -177,9 +124,6 @@ pause()
 
 ## Read in peaksets, identify consensus
 # %>% = pipe
-# https://github.com/andreamrau/OpticRegen_2019/blob/master/1_preprocessing/ATACseq/7_remove_MTduplicates.sh
-# Example OpticRegen filename: ALIGNED_TRANS/12ATAC-3.bam
-# Condition = 12, replicate = 3
 # Example CNC-EMDiff filename: CCA1A_S1_L001-nodup-uniq.bam
 # "1A": 1 = condition, A = time point
 # SampleID <- strsplit(dir("ALIGNED_TRANS/"), split=".", fixed=TRUE) %>%
@@ -187,24 +131,18 @@ pause()
 SampleID <- strsplit(dir("4-bwa-mem/", pattern=paste0(cell_type, ".*.bam")),
   split=".", fixed=TRUE) %>% lapply(., function(x) x[1]) %>%
   unlist() %>% unique()
-# 12ATAC-3
 
 print("SampleID")
 print(SampleID)
 
-# Condition <- strsplit(SampleID, split="ATAC", fixed=TRUE) %>%
-#Condition <- substr(SampleID, 5, 5) %>% unlist()
-Condition <- substr(SampleID, 4, 4) %>% unlist()    # Reverse
-# 12
+# Condition <- substr(SampleID, 5, 5) %>% unlist()
+Condition <- substr(SampleID, 4, 4) %>% unlist()    # Flip condition/replicate
 
 print("Condition")
 print(Condition)
 
-# Replicate <- strsplit(SampleID, split="-", fixed=TRUE) %>%
-#Replicate <- substr(SampleID, 4, 4)%>% unlist()
-Replicate <- substr(SampleID, 5, 5)%>% unlist()     # Reverse
-# %>% as.numeric()
-# 3
+# Replicate <- substr(SampleID, 4, 4)%>% unlist()
+Replicate <- substr(SampleID, 5, 5)%>% unlist()     # Flip condition/replicate
 
 print("Replicate")
 print(Replicate)
@@ -214,24 +152,16 @@ pause()
 # bamReads <- paste0("ALIGNED_TRANS/",
 #                dir("ALIGNED_TRANS/")[-grep("bam.", dir("ALIGNED_TRANS/"))])
 bamReads <- paste0("4-bwa-mem/",
-		 dir("4-bwa-mem/", pattern=paste0(cell_type, ".*.bam$")))
+		   dir("4-bwa-mem/", pattern=paste0(cell_type, ".*.bam$")))
 
 print("bamReads")
 print(bamReads)
 
-# Peaks <- paste0("PEAKS_TRANS_PEAKLETS_ALLTIMES/", Condition, 
-#                 "ATAC.nodup.unique.macs_peaks.pvalsort.narrowPeak.bed")
-# Peaks <- paste0("PEAKS_TRANS_PEAKLETS_ALLTIMES/",
-#                 "ALLMERGED_ATAC.nodup.unique.macs.peaklets_peaks.pvalsort.narrowPeak_501bp.bed")
-
-# Merged peaks generated by Andrea's R code above
+# Merged peaks generated by R code above
 Peaks <- paste0(cell_type, "-merged-peaks.bed")
 
 # Generated externally using bedtools
 # Peaks <- paste0("7-macs-peaklets/high-confidence-p10-", cell_type, "-501-merged.bed")
-
-# Straight from MACS
-# Peaks <- "7-macs-peaklets/ATAC-NCA-macs.peaklets_peaks.narrowPeak"
 
 print("Peaks")
 print(Peaks)
@@ -261,8 +191,9 @@ if ( file.exists(readcounts_filename) ) {
     end_time <- Sys.time()
     print("dba() time:")
     print(end_time - start_time)
-    pdf("peaksets_pvalsort_overall-clustering.pdf")
-    # Debug?
+    
+    # Debug
+    pdf(paste0("-peaksets-pvalsort-overall-clustering", cell_type, ".pdf"))
     plot(peaksets_pvalsort)
     dev.off()   # close() pdf
     
@@ -270,7 +201,6 @@ if ( file.exists(readcounts_filename) ) {
     # Count bam pileups over peaks
     # dba.count() takes a long time so use saved results if available
     # Remove the file before running this script if anything has changed
-
     print("Running dba.count(peaksets_pvalsort)")
     start_time <- Sys.time()
     readcounts_pvalsort <- dba.count(peaksets_pvalsort)
@@ -279,21 +209,21 @@ if ( file.exists(readcounts_filename) ) {
     print(end_time - start_time)
     
     # debug
-    pdf("readcounts_pvalsort_overall-clustering.pdf")
+    pdf(paste0("readcounts-pvalsort-overall-clustering", cell_type, ".pdf")
     plot(readcounts_pvalsort)
     dev.off()
     
     # Save this one object from the session
-    # load("readcounts_pvalsort.RData") to view
     save(list="readcounts_pvalsort", file=readcounts_filename)
     #save(list=c("samples", "readcounts_pvalsort"), file="sampledata.RData")
 }
+
 print("readcounts_pvalsort:")
 print(readcounts_pvalsort)
 pause()
 
 # Does not work: "Cannot coerc" error
-#write.table(readcounts_pvalsort, file="readcounts_pvalsort.table",
+# write.table(readcounts_pvalsort, file="readcounts_pvalsort.table",
 #            quote=FALSE, sep="\t")
 
 ## Differential analysis (DESeq2) comparing all time points to 0
@@ -314,8 +244,6 @@ pause()
 # Organize as a table with each sample as a column
 counts_pvalsort <- lapply(readcounts_pvalsort$peaks, function(x) x$Reads) %>%
   do.call("cbind", .)
-#print("counts_pvalsort:")
-#print(counts_pvalsort)
 
 # Recode X variable as a numeric indicator variable
 # factor level variable instead of character for regression
@@ -352,51 +280,32 @@ print("dds_pvalsort:")
 print(dds_pvalsort)
 pause()
 
-# Adjust this for Maria's time points:
-# 
 # Generate data for Excel file
-## 184 total are differential (down from 208 in previous analysis)
-#res_pvalsort_BvsA <- results(dds_pvalsort, contrast = c("time_factor", "B", "A"))
-#res_pvalsort_CvsA <- results(dds_pvalsort, contrast = c("time_factor", "C", "A"))
-#res_pvalsort_CvsB <- results(dds_pvalsort, contrast = c("time_factor", "C", "B"))
 
-# Reverse
+# According to file naming conventions.  Does not work: Almost no differential
+# peaks.  Are we comparing replicates rather than time points due to a sample
+# mixup?
+# res_pvalsort_BvsA <- results(dds_pvalsort, contrast = c("time_factor", "B", "A"))
+# res_pvalsort_CvsA <- results(dds_pvalsort, contrast = c("time_factor", "C", "A"))
+# res_pvalsort_CvsB <- results(dds_pvalsort, contrast = c("time_factor", "C", "B"))
+
+# Flip condition/replicate
 res_pvalsort_BvsA <- results(dds_pvalsort, contrast = c("time_factor", "2", "1"))
 res_pvalsort_CvsA <- results(dds_pvalsort, contrast = c("time_factor", "3", "1"))
 res_pvalsort_CvsB <- results(dds_pvalsort, contrast = c("time_factor", "3", "2"))
 
-print(paste0(cell_type, " 2 vs 1"))
+print(paste0(cell_type, " day 2 vs day 0"))
 summary(res_pvalsort_BvsA, alpha=0.05) ## Previously 104
-# out of 42198 with nonzero total read count
-# adjusted p-value < 0.05
-# LFC > 0 (up)     : 141, 0.33% 
-# LFC < 0 (down)   : 9, 0.021% 
-# outliers [1]     : 0, 0% 
-# low counts [2]   : 14726, 35% 
-# (mean count < 31)
 
-print(paste0(cell_type, " 3 vs 1"))
+print(paste0(cell_type, " day 6 vs day 0"))
 summary(res_pvalsort_CvsA, alpha=0.05) ## Previously 0
-# out of 42198 with nonzero total read count
-# adjusted p-value < 0.05
-# LFC > 0 (up)     : 1, 0.0024% 
-# LFC < 0 (down)   : 0, 0% 
-# outliers [1]     : 0, 0% 
-# low counts [2]   : 0, 0% 
-# (mean count < 1)
 
-print(paste0(cell_type, " 3 vs 2"))
+print(paste0(cell_type, " day 6 vs day 2"))
 summary(res_pvalsort_CvsB, alpha=0.05)
-# out of 42198 with nonzero total read count
-# adjusted p-value < 0.05
-# LFC > 0 (up)     : 0, 0% 
-# LFC < 0 (down)   : 0, 0% 
-# outliers [1]     : 0, 0% 
-# low counts [2]   : 0, 0% 
-# (mean count < 1)
 stop()
 
-# Create CSVs with differntially accessible peaks
+# Andrea's OpticRegen code below
+# Create CSVs with differentially accessible peaks
 ## No differential peaks at 7 to 0
 for(i in c(2,4,12)) {
   res_pvalsort <- get(paste0("res_pvalsort_", i, "vs0"))
