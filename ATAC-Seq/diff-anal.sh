440a630436554fd259fb22bc5891ced56fd1c005
Jason Bacon <bacon@compute-001.albacore>
2020-04-07 16:53:11 -0500

    Add 10-diff-anal.sbatch

--- ATAC-Seq/diff-anal.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -e
-
-##########################################################################
-#   Prepare input file for loading into R for DESeq2
-##########################################################################
-
-# BAM
-awk '{ printf("%s\t%s\t%s\t%s\n", $1, $2, $3, "chr"$1"-"$2"-"$3) }' \
-    7-macs-peaklets/high-confidence-CCA-500-merged.bed
