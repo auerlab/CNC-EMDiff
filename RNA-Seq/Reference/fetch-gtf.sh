ec4524afc8a4eccc93307a46a5b77b1ec227c135
Jason Bacon <jwb@FreeBSD.org>
2020-06-18 15:56:41 -0500

    Progress on finding proximal peaks Document differences between CDNA and GTF Clean up old transcripts

--- RNA-Seq/Reference/fetch-gtf.sh
+++ RNA-Seq/Reference/fetch-gtf.sh
@@ -5,9 +5,12 @@ fetch="$(./find-fetch.sh)"
 # GTF
 # Can't guarantee this file or the chromosome files will always be available.
 # You may need to edit this.
-gtf=Mus_musculus.GRCm38.98.gtf
+release=$(./reference-release)
+gtf=Mus_musculus.GRCm38.$release.gtf
 if [ ! -e $gtf ]; then
-    $fetch ftp://ftp.ensembl.org/pub/release-98/gtf/mus_musculus/$gtf.gz
+    $fetch ftp://ftp.ensembl.org/pub/release-$release/gtf/mus_musculus/$gtf.gz
     gunzip $gtf.gz
+else
+    printf "$gtf already exists.\nRemove it and run $0 again to replace.\n"
 fi
 
