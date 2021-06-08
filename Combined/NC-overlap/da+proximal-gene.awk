a9a222a7e6b59f5e94d6c44a3f809456bdd6fb59
Jason Bacon <bacon@danio.uits.uwm.edu>
2021-06-08 14:32:47 -0500

    Progress on overlapping peak sets for commonly regulated genes

---
+++ Combined/NC-overlap/da+proximal-gene.awk
@@ -0,0 +1,35 @@
+#############################################################################
+#   Description:
+#       Filter peaks file for peaks with differential accessibility and
+#       proximity to a list of genes
+#
+#   $6 = T1-T0 APV
+#   $8 = T2-T0 APV
+#   $10 = T2-T1 APV
+#
+#   History: 
+#   Date        Name        Modification
+#   2021-06-08  Jason Bacon Begin
+#############################################################################
+
+# In case we want to look at LFC
+function abs(x)
+{
+    return x < 0 ? -x : x;
+}
+
+# If any APV is < 0.05, it's DA
+($6 < 0.05) || ($8 < 0.05) || ($10 < 0.05) {
+    chrom=$1;
+    start=$2;
+    end=$3;
+    gene=$11;
+    split(genestr, genes);
+    # printf("gene = %s\n", gene);
+    for (c in genes) {
+	if ( gene == genes[c] ) {
+	    printf("%s\t%s\t%s\t%s\n%s\t%s\t%s\n",
+		    chrom, start, end, gene, $6, $8, $10);
+	}
+    }
+}
--- Combined/NC-overlap/overlap.sh
+++ Combined/NC-overlap/overlap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-##########################################################################
+##############################################################################
 #   Find overlap of DA peaks from neuro and chondro that are within 250
 #   kb of commonly regulated DE genes
 #
@@ -9,8 +9,38 @@
 #        patterns
 #     3. for each overlap report DA temporal accessibility patterns for
 #        chondro and neuro  2. Find overlap of DA peaks from neuro and chondro that are 
-##########################################################################
+##############################################################################
 
-# Filter peaks for genes in genes-common*.txt and differential accessibility
-# across any time interval
+# Filter peaks for differential accessibility across any time interval
+# and genes in genes-common*.txt
+
+##############################################################################
+#
+# Sample peak input:
+#
+# Chr     Start   End     Peak-name           
+# 1       6272149 6272650 chr1-6272149-6272650
+#
+# T1-T0-LFC    T1-T0-APV   T2-T0-LFC    T2-T0-APV   T2-T1-LFC   T2-T1-APV
+# -0.528       0.755       -0.227       0.689       0.301       0.533
+#
+# Gene-name Gene-Distance
+# St18      214581
+##############################################################################
+
+peaks_dir=../All-peaks-within-X-kb-of-DE-genes/Proximal+distal-250kb
+common_genes="$(cat Data/genes-common-*.txt | sort | uniq)"
+common_genes=$(echo $common_genes)  # Convert newlines to spaces
+echo $common_genes
+printf "%u commonly regulated genes (union of up and down)\n" \
+    $(echo $common_genes | wc -w)
+
+for file in $peaks_dir/genes-CCA-c[1-3].bed-proximal+distal-peaks.tsv; do
+    echo $file
+    awk -v genestr="$common_genes" -f da+proximal-gene.awk $file | head
+done
+for file in $peaks_dir/genes-NCA-c[1-5].bed-proximal+distal-peaks.tsv; do
+    echo $file
+    awk -v genestr="$common_genes" -f da+proximal-gene.awk $file | head
+done
 
