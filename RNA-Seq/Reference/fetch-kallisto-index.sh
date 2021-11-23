3e3f968562faf4f2860897971279a1dca16df0d9
Jason Bacon <bacon@head.albacore>
2021-11-23 11:45:31 -0600

    5-kallisto-quant: Convert xz to gz for kallisto 0.46.1 Clean up other scripts

--- RNA-Seq/Reference/fetch-kallisto-index.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -e
-
-fetch=$(../Common/find-fetch.sh)
-release=$(../Common/kallisto-release.sh)
-kallisto_reference=mus_musculus.tar.gz
-if [ ! -e $kallisto_reference ]; then
-    $fetch https://github.com/pachterlab/kallisto-transcriptome-indices/releases/download/ensembl-$release/$kallisto_reference
-fi
-tar ztf $kallisto_reference
