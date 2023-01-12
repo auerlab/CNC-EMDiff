9401756c4549d4f0e13f29a356c7ae10472af2ef
Jason W. Bacon <bacon4000@gmail.com>
2023-01-12 08:55:12 -0600

    Streamline inspect-results.sh

--- RNA-Seq/inspect-results.sh
+++ RNA-Seq/inspect-results.sh
@@ -56,21 +56,88 @@ pause()
 #   2023-01-12  Jason Bacon Begin
 ##########################################################################
 
-view_logs()
+run_cmd()
 {
     if [ $# != 1 ]; then
-	printf "Usage: view_logs dir\n" >> /dev/stderr
+	printf 'Usage: run_cmd "command"\n' >> /dev/stderr
 	exit 1
     fi
     
-    dir=$1
-    for log in Logs/$dir/*; do
-	more $log
-	printf "View another? [y]/n "
-	read another
-	if [ 0$another = 0n ]; then
-	    break;
-	fi
+    printf "Running '%s':\n" "$1"
+    pause
+    eval $1
+    return 0
+}
+
+
+##########################################################################
+#   Function description:
+#       
+#   Arguments:
+#       
+#   Returns:
+#       
+#   History:
+#   Date        Name        Modification
+#   2023-01-12  Jason Bacon Begin
+##########################################################################
+
+view_files()
+{
+    if [ $# -lt 1 ]; then
+	printf "Usage: view_files file-spec\n" >> /dev/stderr
+	exit 1
+    fi
+    
+    for file in $@; do
+	printf "\n(V)iew $file\n(S)kip $file (default)\n(Q)uit\n"
+	read view
+	case $view in
+	V|v)
+	    more $file
+	    ;;
+	
+	Q|q)
+	    break
+	    ;;
+	esac
+    done
+    return 0
+}
+
+
+##########################################################################
+#   Function description:
+#       
+#   Arguments:
+#       
+#   Returns:
+#       
+#   History:
+#   Date        Name        Modification
+#   2023-01-12  Jason Bacon Begin
+##########################################################################
+
+wb_files()
+{
+    if [ $# -lt 1 ]; then
+	printf "Usage: wb_files file-spec\n" >> /dev/stderr
+	exit 1
+    fi
+
+    for file in $@; do
+	printf "\n(V)iew $file?\n(S)kip $file (default)\n(Q)uit\n"
+	read view
+	case $view in
+	V|v)
+	    webbrowser $file
+	    ;;
+	
+	Q|q)
+	    break
+	    ;;
+	
+	esac
     done
     return 0
 }
@@ -100,7 +167,10 @@ while [ 0$selection != 0q ]; do
 1.. Raw renamed links
 2.. Raw FastQC reports
 3.. Raw MultiQC report
-4.. FastQ trim
+4.. Adapter trimming
+5.. Trimmed FastQC reports
+6.. Trimmed MultiQC report
+7.. Reference transcriptome
 Q.. Quit
 
 EOM
@@ -109,33 +179,26 @@ EOM
     
     case $selection in
     1)
-	ls -l Data/01-organize/Raw-renamed | more
+	run_cmd "ls -l Data/01-organize/Raw-renamed | more"
 	;;
     
     2)
-	view_logs 02-fastqc-raw
-	for report in Data/02-qc-raw/*.html; do
-	    webbrowser $report
-	    printf "View another? [y]/n "
-	    read another
-	    if [ 0$another = 0n ]; then
-		break;
-	    fi
-	done
+	view_files Logs/02-qc-raw/*
+	wb_files Data/02-qc-raw/*.html
 	;;
     
     3)
-	more Logs/03-multiqc-raw/*
-	webbrowser ./Data/03-multiqc-raw/multiqc_report.html
+	view_files Logs/03-multiqc-raw/*
+	run_cmd "webbrowser ./Data/03-multiqc-raw/multiqc_report.html"
 	;;
 
     4)
-	view_logs 04-trim
+	view_files Logs/04-trim/*
 	printf "Check for remaining adapter contamination? y/[n] "
 	read scum
 	if [ 0$scum = 0y ]; then
 	    for fq in Data/04-trim/*.gz; do
-		$srun fastq-scum $fq
+		run_cmd "$srun fastq-scum $fq"
 		printf "View another? [y]/n "
 		read another
 		if [ 0$another = 0n ]; then
@@ -145,10 +208,28 @@ EOM
 	fi
 	;;
     
+    5)
+	view_files Logs/05-qc-trimmed/*
+	wb_files Data/05-qc-trimmed/*.html
+	;;
+    
+    6)
+	view_files Logs/06-multiqc-trimmed/*
+	run_cmd "webbrowser ./Data/06-multiqc-trimmed/multiqc_report.html"
+	;;
+
+    7)
+	view_files Logs/07-reference/*
+	(cd Data/07-reference && view_files *.fa *.fai *.tsv)
+	;;
+    
+    Q|q)
+	;;
+    
     *)
 	printf "Invalid selection #: $selection\n" >> /dev/stderr
-	exit 1
 	;;
+
     esac
     pause
 done
