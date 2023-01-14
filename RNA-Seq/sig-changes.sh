103395a8a803dad4be9b1ebc422d24a6d4862d67
Jason W. Bacon <bacon4000@gmail.com>
2023-01-14 09:34:10 -0600

    sig-changes.sh: Frame out basic results filtering

--- RNA-Seq/sig-changes.sh
+++ RNA-Seq/sig-changes.sh
@@ -32,6 +32,20 @@ usage()
 
 
 ##########################################################################
+#   Function description:
+#       Pause until user presses return
+##########################################################################
+
+pause()
+{
+    local junk
+    
+    printf "Press return to continue..."
+    read junk
+}
+
+
+##########################################################################
 #   Main
 ##########################################################################
 
@@ -41,4 +55,105 @@ fi
 
 # Possibly use GAF files directly, or tools sets like goatools or ermineJ
 # wget http://geneontology.org/gene-associations/goa_human.gaf.gz
+
+# 
+while true; do
+    clear
+    cat << EOM
+
+1.. Browse Kallisto DE results
+2.. Filter Kallisto DE results
+Q.. Quit
+
+EOM
+    printf "Selection? "
+    read selection
+    
+    case 0$selection in
+    01)
+	more Data/13-fasda-kallisto/*.txt
+	;;
+    
+    02)
+	cat << EOM
+			    **** NOTE ****
+			    
+What constitutes a meaningful read count varies immensely from one gene
+to another.  It also depends directly on the count of DNA/RNA fragments
+in the sample, which does not reflect the count per cell.  Minimum high
+read count is a arbitrary decision unless you know the parameters of
+your read data and genes of interest.  Use with caution.
+
+What is a meaningful fold-change also varies immensely among genes.
+1.5 might be meaningful for an enzyme whose affect is amplified, and
+meaningless for genes whose translation level is limited by other factors.
+
+P-values represent STATISTICAL significance of a fold-change based on
+unreliable read counts, not BIOLOGICAL significance of a change in the cell.
+Read counts are highly unreliable due to the many uncontrollable variables
+in the cell and the sequencing process that distort read counts.
+
+Users should not rely on any one set of parameters here to determine
+which fold-changes are biologically significant.
+
+EOM
+	printf "Minimum high read count? [30] "
+	read min_count
+	if [ -z "$min_count" ]; then
+	    min_count=30
+	fi
+
+	printf "Minimum %% agreement across replicates for up/down regulation? [66] "
+	read min_agree
+	if [ -z "$min_agree" ]; then
+	    min_agree=66
+	fi
+	
+	printf "Mininum FC? [2] "
+	read min_fc
+	if [ -z "$min_fc" ]; then
+	    min_fc=2
+	fi
+	
+	printf "Maximum P-value? [0.2] "
+	read max_pv
+	if [ -z "$max_pv" ]; then
+	    max_pv=0.2
+	fi
+
+	printf "%-30s %-s\n" "File" "Features meeting criteria"
+	for file in Data/13-fasda-kallisto/*.txt; do
+	    printf "%-30s" `basename $file`
+	    awk -v min_count=$min_count \
+		-v min_agree=$min_agree \
+		-v min_fc=$min_fc \
+		-v max_pv=$max_pv \
+		'(($2 >= min_count) || ($3 >= min_count)) && ($6 >= min_agree) && ($7 >= min_fc) && ($8 <= max_pv)' \
+		$file | wc -l
+	done
+	printf "\nView results? [y]/n "
+	read view
+	if [ 0$view != 0n ]; then
+	    for file in Data/13-fasda-kallisto/*.txt; do
+		basename $file
+		awk -v min_count=$min_count \
+		    -v min_agree=$min_agree \
+		    -v min_fc=$min_fc \
+		    -v max_pv=$max_pv \
+		    '(($2 >= min_count) || ($3 >= min_count)) && ($6 >= min_agree) && ($7 >= min_fc) && ($8 <= max_pv)' \
+		    $file | more
+	    done
+	fi
+	;;
+    
+    0Q|0q)
+	exit 0
+	;;
+    
+    *)
+	printf "Invalid selection.\n"
+	pause
+    
+    esac
+done
 
