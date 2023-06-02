c5fa943b229ed4b8a0dcdb3157935b5c12c0a027
Jason W. Bacon <bacon4000@gmail.com>
2023-06-02 15:42:23 -0500

    02-qc xargs: Record log in Logs and check for results before processing

--- RNA-Seq/Xargs/02-qc-raw.sh
+++ RNA-Seq/Xargs/02-qc-raw.sh
@@ -3,10 +3,14 @@
 # Raw files from sequencing center
 raw=$1
 
-printf "fastqc $raw...\n"
-
 # Filename stems for fastqc output
 stem_raw=$(basename ${raw%.fastq.xz})
+results=Results/02-qc-raw/$(basename ${stem_raw})_fastqc.html
 
-xzcat $raw | fastqc -o Results/02-qc-raw stdin:$stem_raw
-
+if [ -e $results ]; then
+    printf "$raw already processed.\n"
+else
+    printf "Processing $raw with fastqc...\n"
+    xzcat $raw | fastqc -o Results/02-qc-raw stdin:$stem_raw \
+	> Logs/02-qc-raw/$stem_raw.out 2> Logs/02-qc-raw/$stem_raw.err
+fi
