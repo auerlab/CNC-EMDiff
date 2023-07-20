b574a7f832f024f5ec1849c8dd41469d8adf8d80
Jason W. Bacon <bacon4000@gmail.com>
2023-07-20 10:52:20 -0500

    kallisto-vs-hisat2: Expand comparisons and clean up output

--- RNA-Seq/kallisto-vs-hisat2.sh
+++ RNA-Seq/kallisto-vs-hisat2.sh
@@ -14,23 +14,34 @@ pause()
     read junk
 }
 
-# Abundances
-kallisto=Results/13-fasda-kallisto/chondro-time1-all-norm.tsv
-hisat2=Results/20-fasda-fc-hisat2/chondro-time1-all-norm.tsv
-for transcript in $(awk '{ print $1 }' $kallisto | head -20); do
+# Raw counts
+kallisto=Results/09-kallisto-quant/chondro-sample1-rep1-time1/abundance.tsv
+hisat2=Results/19-fasda-abundance-hisat2/chondro-sample1-rep1-time1-abundance.tsv
+for transcript in $(awk '{ print $1 }' $kallisto); do
     echo '==='
     printf "Kallisto: "
     grep $transcript $kallisto
     printf "Hisat2:   "
     grep $transcript $hisat2
-done
-exit
+done | more
+
+# Normalized counts
+kallisto=Results/13-fasda-kallisto/chondro-time1-all-norm.tsv
+hisat2=Results/20-fasda-fc-hisat2/chondro-time1-all-norm.tsv
+for transcript in $(awk '{ print $1 }' $kallisto); do
+    echo '==='
+    printf "Kallisto: "
+    grep $transcript $kallisto
+    printf "Hisat2:   "
+    grep $transcript $hisat2
+done | more
 
 # Fold-changes
 kallisto=Results/13-fasda-kallisto/chondro-time1-time2-FC.txt
 hisat2=Results/20-fasda-fc-hisat2/chondro-time1-time2-FC.txt
-for transcript in $(awk '{ print $1 }' $kallisto | head -20); do
-    echo $transcript
+for transcript in $(awk '{ print $1 }' $kallisto); do
+    printf "\n"
+    head -n 1 $kallisto
     grep $transcript $kallisto
     grep $transcript $hisat2
-done
+done | more
