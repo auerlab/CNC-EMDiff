5fe96732d549d44a6e2986a9878e0b6b1799de04
Jason W. Bacon <bacon4000@gmail.com>
2023-12-30 20:11:25 -0600

    ATAC-Seq: Add no cluster bwa align scripts

---
+++ ATAC-Seq/No-cluster/09-bwa-align.sh
@@ -0,0 +1,37 @@
+#!/bin/sh -e
+
+##########################################################################
+#   Script description:
+#       Use hisat2 to align reads to a genome reference.
+#
+#   Dependencies:
+#       Requires trimmed reads and a reference genome.  Run after
+#       *-trim.sh and *-reference.sh.
+##########################################################################
+
+hw_threads=$(../../Common/get-hw-threads.sh)
+hw_mem=$(../../Common/get-hw-mem.sh)
+hw_gib=$(( $hw_mem / 1024 / 1024 / 1024 ))
+
+# For xenopus, hisat2 jobs take about 4.3 GB
+# For axolotl, quite a bit more
+if pwd | fgrep XenONReg; then
+    jobs=$(( $hw_gib / 6 ))
+else
+    jobs=$(( $hw_gib / 16 ))
+fi
+threads_per_job=$(( $hw_threads / $jobs ))
+
+# Use at least 4 thread per job, since the jobs will run almost 4 times
+# as fast, and fewer jobs means less disk contention.  If CPU utilization
+# is less than 90% per core (360% for a 4-thread job), you probably have
+# too much disk contention and need to reduce the number of jobs.
+if [ $threads_per_job -lt 4 ]; then
+    threads_per_job=4
+    jobs=$(( $hw_threads / $threads_per_job ))
+fi
+
+# Tried GNU parallel and ran into bugs.  Xargs just works.
+ls Results/05-trim/*-R1.fastq.zst | \
+    xargs -n 1 -P $jobs ../../Common/redirect.sh \
+    Sh/09-bwa-align.sh $threads_per_job
---
+++ ATAC-Seq/No-cluster/Sh/09-bwa-align.sh
@@ -0,0 +1,54 @@
+#!/bin/sh -e
+
+##########################################################################
+#   Script description:
+#       Align reads to reference genome
+#
+#   Usage:
+#       SLURM cluster:
+#           sbatch 09-bwa-mem.sbatch
+#
+#   History:
+#       Based on the work of Dr. Andrea Rau:
+#       https://github.com/andreamrau/OpticRegen_2019
+#   Date        Name        Modification
+#   2020-02-20  Jason Bacon Begin
+##########################################################################
+
+##########################################################################
+#   Main
+##########################################################################
+
+if [ $# != 2 ]; then
+    printf "Usage: $0 threads-per-job input fasta-R1\n"
+    exit 1
+fi
+threads_per_job=$1
+infile1=$2
+infile2=$(echo $infile1 | sed -e 's|R1|R2|')
+
+# Document software versions used for publication
+uname -a
+bwa || true
+pwd
+
+ref_file=$(../../RNA-Seq/Reference/genome-filename.sh)
+
+# One iteration if running under SLURM, all iterations otherwise
+cd Results/09-bwa-align
+outfile=$(basename ${infile1%-R1.fastq.gz}).sam
+
+set -x
+pipe1=$(basename $infile1 | sed -e 's|zst|fifo|')
+pipe2=$(basename $infile2 | sed -e 's|zst|fifo|')
+rm -f $pipe1 $pipe2
+mkfifo $pipe1 $pipe2
+zstdcat ../../$infile1 > $pipe1 &
+zstdcat ../../$infile2 > $pipe2 &
+
+set -x
+bwa mem -M -t $threads_per_job \
+    ../08-bwa-index/$ref_file $pipe1 $pipe2 > $outfile
+
+wait
+rm -f $pipe1 $pipe2
