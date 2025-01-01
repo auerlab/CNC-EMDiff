e1cd9c2159be0a24b2cb7bd228d4498fa90061da
Jason W. Bacon <bacon4000@gmail.com>
2025-01-01 09:13:22 -0600

    hisat2-index: Adjust memory requirements per chaperone report

--- RNA-Seq/Utils/install-R-packages.sh
+++ RNA-Seq/Utils/install-R-packages.sh
@@ -1,7 +1,11 @@
 #!/bin/sh -e
 
+# R_LIBS_USER should be set to shared-path/R/library in ~/.Renviron
+r_libs_user=~/R/library
+printf "R_LIBS_USER=$r_libs_user\n" >> ~/.Renviron
+
 # Not surprisingly, using dir.create() in the R script doesn't work.
 # The directory is not accessible by the same Rscript process, though
 # is is by subsequent runs.  :-/
-mkdir -p ~/R/library
+mkdir -p $r_libs_user
 Utils/install-R-packages.R
