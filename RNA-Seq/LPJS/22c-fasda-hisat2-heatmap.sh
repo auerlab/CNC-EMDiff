e4ea42e329d257dde7cca8609e61dbaa90ff1db0
Jason W. Bacon <bacon4000@gmail.com>
2025-06-11 15:18:00 -0500

    RNA-Seq/LPJS: Add example heatmap script for hisat2

---
+++ RNA-Seq/LPJS/22c-fasda-hisat2-heatmap.sh
@@ -0,0 +1,36 @@
+#!/bin/sh -e
+
+##########################################################################
+#   Description:
+#       Generate and display heatmap for Kallisto results
+#       
+#   History:
+#   Date        Name        Modification
+#   2025-03-31  Jason Bacon Begin
+##########################################################################
+
+input_dir=Results/17-hisat2-align
+output_dir=Results/22-fasda-fc-hisat2
+samples=$(ls -d $input_dir/chondro*.bam | wc -l)
+replicates=$(($samples / 3))
+
+for cell_type in chondro neuro; do
+    for start_time in $(seq 1 2); do
+	for end_time in $(seq $((start_time + 1)) 3); do
+	    printf "Comparing time$start_time and time$end_time...\n"
+	    # Generate a list of features of interest by filtering the FASDA fold-changes
+	    features=filtered-features.txt
+	    # chondro time2-time3 has nothing with P < 0.05 (odd)
+	    fasda filter --max-p-val 0.10 \
+		$output_dir/$cell_type-time$start_time-time$end_time-FC.txt \
+		| awk '{ print $1 }' | head -n 30 > $features
+	    wc -l $features
+	    
+	    # FIXME: Update when ./heatmap is moved into PATH
+	    # Add --debug to see Python data structures
+	    fasda heatmap $features \
+		$output_dir/$cell_type-time$start_time-norm.tsv \
+		$output_dir/$cell_type-time$end_time-norm.tsv
+	done
+    done
+done
